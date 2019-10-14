0c93ba94cb78ee15f3f086d56e8bb6d8d2a3e2ea
Víctor Martínez Montané <victorcoder2@gmail.com>
2019-10-14 21:03:35 +0200

    Update Partitioner.java

--- src/Tarea3/Partitioner.java
+++ src/Tarea3/Partitioner.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class Partitioner {
 
-    public static <E extends Comparable<E>> void copyPartition(List<? extends E> src, E pivot, List<? super E> trg1, List<? super E> trg2){
+    public static <E extends Comparable<? super E>> void copyPartition(List<? extends E> src, E pivot, List<? super E> trg1, List<? super E> trg2){
         for (E elem: src) {
             if(elem.compareTo(pivot) <= 0) trg1.add(elem);
             else trg2.add(elem);
